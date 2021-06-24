e407cd81ae9caa39c886438f805e4676d05b4c10
Emma Huang <emmahuang.ty@gmail.com>
2021-06-24 16:16:02 -0400

    fixed michelle's tooltip

--- Michelle/vis2.js
+++ Michelle/vis2.js
@@ -104,7 +104,7 @@ const render = function(data) {
         var mousemove = function(d) {
             tooltip
                 .style("left", (d.clientX + 10) + "px")
-                .style("top", (d.clientY - 300) + "px")
+                .style("top", (d.clientY - svgHeight + 130) + "px")
         }
 
         var mouseleave = function(d) {
